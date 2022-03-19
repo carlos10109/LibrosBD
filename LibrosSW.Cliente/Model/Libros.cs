b71262cd944dd8cddf7d084db84049541d77fbb8
Carlos Daniel Arcos <47263414+carlos10109@users.noreply.github.com>
2022-03-18 19:38:21 -0600

    Add files via upload

---
+++ LibrosSW.Cliente/Model/Libros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrosSW.Cliente.Model
+{
+    public class Libros
+    {
+        public int IdLibro { get; set; }
+        public string Titulo { get; set; }
+        public string FechaLanzamiento { get; set; }
+        public int Autor { get; set; }
+        public int Categoria { get; set; }
+        public int Editorial { get; set; }
+        public string Idioma { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
